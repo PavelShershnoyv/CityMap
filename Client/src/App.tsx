e5084de5e001721ef82ff12c4fdf093c789aeff5
PavelShershnoyv <pavel.shershnoyv.02@gmail.com>
2022-06-15 14:40:53 +0500

    Create about and update header

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,26 +1,28 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Layout } from "./components/layout/layout";
-import {AuthPage} from "./components/Authorization/AuthPage";
-import {DefaultPlacemarks} from './components/maps/DefaultPlacemarks';
+import { AuthPage } from "./components/Authorization/AuthPage";
+import { DefaultPlacemarks } from "./components/maps/DefaultPlacemarks";
 import Registration from "./components/Authorization/Registration";
 import Login from "./components/Authorization/Login";
 import "./App.css";
-import {ProposalPlacemarks} from "./components/maps/ProposalPlacemarks";
-import {Events} from "./components/maps/Events";
+import { ProposalPlacemarks } from "./components/maps/ProposalPlacemarks";
+import { Events } from "./components/maps/Events";
+import { About } from "./components/About/About";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Navigate to="present" />}/>
+          <Route index element={<Navigate to="present" />} />
           <Route path=":type" element={<DefaultPlacemarks />} />
           <Route path="proposals" element={<ProposalPlacemarks />} />
           <Route path="events" element={<Events />} />
         </Route>
+        <Route path="/about" element={<About />} /> 
         <Route path="/account" element={<AuthPage />}>
-          <Route index element={<Navigate to="login" />}/>
-          <Route path='login' element={<Login />} />
+          <Route index element={<Navigate to="login" />} />
+          <Route path="login" element={<Login />} />
           <Route path="registration" element={<Registration />} />
         </Route>
       </Routes>
--- Client/src/components/header/header.tsx
+++ Client/src/components/header/header.tsx
@@ -1,28 +1,92 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 /* import Arms from "../../images/Arms.svg"; */
 import styles from "./header.module.css";
-import { NavLink } from 'react-router-dom';
-import { UserOutlined } from '@ant-design/icons';
-import './active.css';
+import { NavLink } from "react-router-dom";
+import { UserOutlined } from "@ant-design/icons";
+import "./active.css";
+
+// export const Header: React.FC = () => {
+//   return (
+//     <div className={styles.header}>
+//       {/*<div className={styles.}>*/}
+//       {/*    <img src={Arms} alt="Logo" style={{ width: '70px' }} />*/}
+//       {/*</div>*/}
+//       <div className={styles.navigation}>
+//         <NavLink to="/present" className={styles.navElem}>
+//           Обычная карта
+//         </NavLink>
+//         <NavLink to="/past" className={styles.navElem}>
+//           Реж прошлого
+//         </NavLink>
+//         <NavLink to="/proposals" className={styles.navElem}>
+//           Реж будущего
+//         </NavLink>
+//         <NavLink to="/user" className={styles.navElem}>
+//           Моя карта
+//         </NavLink>
+//         <NavLink to="/events" className={styles.navElem}>
+//           События
+//         </NavLink>
+//         <NavLink to="/about" className={styles.navElem}>
+//           Про Реж
+//         </NavLink>
+//       </div>
+//       <button className={styles.headerBtn}>
+//         <NavLink to="/account/login">
+//           <UserOutlined className={styles.icon} />
+//         </NavLink>
+//       </button>
+//     </div>
+//   );
+// };
+
+import RezLogo from "../../Rez.svg";
+import arms from "../../logo1.svg";
 
 export const Header: React.FC = () => {
-    return (
-        <div className={styles.header}>
-            {/*<div className={styles.}>*/}
-            {/*    <img src={Arms} alt="Logo" style={{ width: '70px' }} />*/}
-            {/*</div>*/}
-            <div className={styles.navigation}>
-                <NavLink to="/present" className={styles.navElem}>Обычная карта</NavLink>
-                <NavLink to="/past" className={styles.navElem}>Реж прошлого</NavLink>
-                <NavLink to="/proposals" className={styles.navElem}>Реж будущего</NavLink>
-                <NavLink to="/user" className={styles.navElem}>Моя карта</NavLink>
-                <NavLink to="/events" className={styles.navElem}>События</NavLink>
-            </div>
-            <button className={styles.headerBtn}>
-                <NavLink to="/account/login">
-                    <UserOutlined className={styles.icon} />
-                </NavLink>
-            </button>
+  return (
+    <div className={styles.header}>
+      {/*<div className={styles.}>*/}
+      {/*    <img src={Arms} alt="Logo" style={{ width: '70px' }} />*/}
+      {/*</div>*/}
+      <div className={styles.navigation}>
+        <div className={styles.mainLogo}>
+          <div className={styles.logo}>
+            <img src={RezLogo} alt="" />
+          </div>
+          <div className={styles.arms}>
+            <img src={arms} alt="" />
+          </div>
         </div>
-    )
-}
+        <div className={styles.nav}>
+          <NavLink to="/present" className={styles.navElem}>
+            Реж <br /> прошлого
+          </NavLink>
+          <NavLink to="/past" className={styles.navElem}>
+            Реж <br /> настоящего
+          </NavLink>
+          <NavLink to="/proposals" className={styles.navElem}>
+            Реж <br /> будущего
+          </NavLink>
+          <NavLink to="/user" className={styles.navElem}>
+            Моя <br /> карта
+          </NavLink>
+          <NavLink to="/events" className={styles.navElem}>
+            <div style={{alignSelf: "center"}}>События</div>
+          </NavLink>
+          <NavLink to="/about" className={styles.navElem} >
+            <div style={{alignSelf: "center"}}>Про Реж</div>
+          </NavLink>
+        </div>
+      </div>
+      <div className={styles.button}>
+        <p>Войти</p>
+        <button className={styles.headerBtn}>
+          <NavLink to="/account/login">
+            <UserOutlined className={styles.icon} />
+          </NavLink>
+        </button>
+      </div>
+    </div>
+  );
+};
