55575d77f4d77509f49d1ebff77e76f2956cc182
DESKTOP-JIA02LP\Raph <abdyev_r@mail.ru>
2022-07-04 10:59:04 +0500

    remove about

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -5,7 +5,6 @@ import {AuthPage} from "./components/Authorization/AuthPage";
 import {UserPlacemarks} from './components/maps/UserPlacemarks';
 import Registration from "./components/Authorization/Registration";
 import Login from "./components/Authorization/Login";
-import {About} from "./components/About/About";
 import {Placemarks} from "./components/maps/Placemarks";
 import "./App.css";
 
@@ -20,7 +19,6 @@ function App() {
                     </Route>
                     <Route path=":type" element={<Placemarks/>}/>
                 </Route>
-                <Route path="/about" element={<About/>}/>
                 <Route path="/account" element={<AuthPage/>}>
                     <Route index element={<Navigate to="login"/>}/>
                     <Route path="login" element={<Login/>}/>
--- Client/src/components/header/header.tsx
+++ Client/src/components/header/header.tsx
@@ -1,48 +1,44 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 /* import Arms from "../../images/Arms.svg"; */
 import styles from "./header.module.css";
-import { NavLink } from "react-router-dom";
-import { UserOutlined } from "@ant-design/icons";
+import {NavLink} from "react-router-dom";
+import {UserOutlined} from "@ant-design/icons";
 import "./active.css";
 import RezLogo from "../../Rez.svg";
 import arms from "../../logo1.svg";
-import { useAuth } from "../../hooks/useAuth";
+import {useAuth} from "../../hooks/useAuth";
 
 export const Header: React.FC = () => {
-    const { isAuthenticated, user } = useAuth();
+    const {isAuthenticated, user} = useAuth();
 
     return (
         <div className={styles.header}>
-            {/*<div className={styles.}>*/}
-            {/*    <img src={Arms} alt="Logo" style={{ width: '70px' }} />*/}
-            {/*</div>*/}
             <div className={styles.navigation}>
-                <div className={styles.mainLogo}>
-                    <div className={styles.logo}>
-                        <img src={RezLogo} alt="" />
-                    </div>
-                    <div className={styles.arms}>
-                        <img src={arms} alt="" />
-                    </div>
-                </div>
                 <div className={styles.nav}>
                     <NavLink to="/present" className={styles.navElem}>
-                        Реж <br /> настоящего
+                        <div className={styles.inner}>
+                            Реж настоящего
+                        </div>
                     </NavLink>
                     <NavLink to="/past" className={styles.navElem}>
-                        Реж <br /> прошлого
+                        <div className={styles.inner}>
+                            Реж прошлого
+                        </div>
                     </NavLink>
                     <NavLink to="/proposals" className={styles.navElem}>
-                        Реж <br /> будущего
+                        <div className={styles.inner}>
+                            Реж будущего
+                        </div>
                     </NavLink>
                     {isAuthenticated && <NavLink to="/user" className={styles.navElem}>
-                        Моя <br /> карта
+                        <div className={styles.inner}>
+                            Моя карта
+                        </div>
                     </NavLink>}
                     <NavLink to="/events" className={styles.navElem}>
-                        <div style={{ alignSelf: "center" }}>События</div>
-                    </NavLink>
-                    <NavLink to="/about" className={styles.navElem} >
-                        <div style={{ alignSelf: "center" }}>Про Реж</div>
+                        <div className={styles.inner}>
+                            События
+                        </div>
                     </NavLink>
                 </div>
             </div>
@@ -50,7 +46,7 @@ export const Header: React.FC = () => {
                 <p>{isAuthenticated ? user?.firstName : 'Войти'}</p>
                 <button className={styles.headerBtn}>
                     <NavLink to="/account/login">
-                        <UserOutlined className={styles.icon} />
+                        <UserOutlined className={styles.icon}/>
                     </NavLink>
                 </button>
             </div>
